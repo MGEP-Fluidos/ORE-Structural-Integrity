792b86f6684c6cc8ccc6d0b52d835b7d757e2dee
Eguzkiñe Martinez-Puente <145550615+eguzmartinez@users.noreply.github.com>
2024-04-16 16:51:37 +0200

    Update Spectral_methods.py

--- Spectral models/Spectral_methods.py
+++ Spectral models/Spectral_methods.py
@@ -11,7 +11,6 @@ from scipy import integrate, special
 from scipy.optimize import fsolve
 
 
-
 def per_spectral_mom(f, Pxx, n):
     '''
     Function that calculates the spectral moments
@@ -90,7 +89,7 @@ def Wirsching_Light(k, Mo):
     rho_WL = a + (1 - a) * (1 - eps)**b
     return rho_WL
 
-def Tovo_Benasciutti(Mo, m, S):
+def Tovo_Benasciutti(Mo, k):
     '''
     Function that calculates correction coefficients to apply the Tovo & Benasciutti
     approach with the nonlinear coefficient proposed by Cianetti
@@ -99,7 +98,7 @@ def Tovo_Benasciutti(Mo, m, S):
     alfa_2 = Mo[2] / np.sqrt(Mo[0] * Mo[4])
     b = ((alfa_1 - alfa_2) * (1.112 * (1 + alfa_1 * alfa_2 - (alfa_1 + alfa_2))
          * np.exp(2.11 * alfa_2) + (alfa_1 - alfa_2))) / (alfa_2 - 1)**2
-    rho_TB = (b + (1 - b) * alfa_2**(m - 1))
+    rho_TB = (b + (1 - b) * alfa_2**(k - 1))
     return rho_TB
 
 
@@ -115,6 +114,30 @@ def Ortiz_Chen(k, Mo, f, Pxx):
     alfa2 = Mo[2] / np.sqrt(Mo[0] * Mo[4])
     rho_OC = betha**k / alfa2
     return rho_OC
+
+def Modified_Tovo_Benasciutti(Mo, k, S):
+    '''
+    Function that calculates correction coefficients to apply the Tovo & Benasciutti
+    approach with the nonlinear coefficient proposed by Cianetti
+    '''
+    def P_values(k):
+        if k == 3:
+            P = [1.021, 0.815, 2.038, 1.003, -0.274, -0.019, 0.212, 0.048]
+        elif k == 4:
+            P = [0.975, 1.591, 2.923, 1.020, -0.287, -0.024, 0.262, 0.052]
+        elif k == 5:
+            P = [0.938, 2.235, 3.661, 1.032, -0.293, -0.029, 0.311, 0.054]
+        elif k == 6:
+            P = [0.913, 2.820, 4.325, 1.036, -0.280, -0.035, 0.357, 0.054]
+        return P
+    P = P_values(k)
+    alfa_1 = Mo[1] / np.sqrt(Mo[0] * Mo[2])
+    alfa_2 = Mo[2] / np.sqrt(Mo[0] * Mo[4])
+    g3 = st.skew(S)
+    g4 = st.kurtosis(S, fisher=False)
+    b = ((alfa_1 - alfa_2) * (P[0] + P[1]*alfa_2 - P[2]*alfa_1*alfa_2)) / (1 - P[3]*alfa_2)**2 * (P[4]*g3**2 + P[5]*g4**2 + P[6]*g4 + P[7]*g3**2*g4 + 1 - 9*P[5] - 3*P[6])
+    rho_MTB = (b + (1 - b) * alfa_2**(k - 1))
+    return rho_MTB
 
 
 ###############################################################################
@@ -230,6 +253,42 @@ def Zhao_Baker(k, C, Mo, Pxx, f):
     d = vp * (2 * np.sqrt(Mo[0]))**k / C * (w * a**(-k / b) * gamma_func(
         1 + k / b) + (1 - w) * np.sqrt(2)**k * gamma_func(1 + k / 2))
     return d[0]
+
+
+def wu(k, C, Mo):
+    
+    def get_a_value(a, k):
+        table = {
+            'a00': {3: 29.7, 4: 124, 5: 570, 7: 15000},
+            'a10': {3: 0.2, 4: -43, 5: -510, 7: -34000},
+            'a11': {3: -10, 4: -19, 5: 100, 7: 17000},
+            'a20': {3: -198.3, 4: -1146, 5: -6100, 7: -172000},
+            'a21': {3: 200, 4: 1323, 5: 7960, 7: 283000},
+            'a22': {3: -34.2, 4: -315, 5: -2270, 7: -103000},
+            'a30': {3: -52.1, 4: -371, 5: -2300, 7: -84000},
+            'a31': {3: 294, 4: 1872, 5: 10700, 7: 344000},
+            'a32': {3: -265.4, 4: -1774, 5: -10590, 7: -367000},
+            'a33': {3: 36, 4: 348, 5: 2430, 7: 101000}
+        }
+        
+        return table.get(a, {}).get(k)
+    
+    alfa_1 = Mo[1] / np.sqrt(Mo[0] * Mo[2])
+    alfa_2 = Mo[2] / np.sqrt(Mo[0] * Mo[4])
+    
+    e1 = np.sqrt(1 - alfa_1**2)
+    e2 = np.sqrt(1 - alfa_2**2)
+    
+    Mk = 0
+    
+    for i in range(0, 4):
+        for j in range(0, i+1):
+            a = get_a_value(f'a{i}{j}', k)
+            Mk += a * e1**(i-j) * e2**j
+    
+    d = (Mo[0])**(k/2) / (C*2*np.pi) * np.sqrt(Mo[4]/Mo[2]) * Mk 
+    
+    return d
 
 
 ###############################################################################
@@ -484,6 +543,24 @@ def Low(k, C, Mo, Mo_l, Mo_w, ZUCF, ZUCF_l, ZUCF_w):
 
     return d
 
+def Han_Ma(k, C, v0, Mo_w, Mo_l, v0_l, v0_w):
+    '''
+    Function that returns the fatigue damage according to the Han-Ma approach
+    [A practical method for combination of fatigue damage subjected to 
+    low–frequency and high–frequency Gaussian random processes
+    doi = {10.1016/j.apor.2016.08.007}]
+    '''   
+    # LF Narrowband damage
+    d_NB_lf = Narrowband(k, C, Mo_l, v0_l)
+    
+    # HF Narrowband damage
+    d_NB_hf = Narrowband(k, C, Mo_w, v0_w)
+    
+    # Damage combination
+    d = (d_NB_lf**(2/k) + d_NB_hf**(2/k))**(k/2)
+    
+    return d
+
 def Gao_Moan(k, C, v0, Mo_h, Mo_l, v0_l, v0_h, Mo_m = 0, v0_m = 0, signal = 'Trimodal'):
     
     if signal == 'Bimodal':
@@ -514,6 +591,78 @@ def Gao_Moan(k, C, v0, Mo_h, Mo_l, v0_l, v0_h, Mo_m = 0, v0_m = 0, signal = 'Tri
         return d_h + d_p + d_q
 
 
+def Double_frequency_coupled(k, C, Mo_w, Mo_l, v0_w, v0_l, Pxx_w, f_w):
+    """
+    Function to calculate double frequency coupled damage.
+
+    Input Parameters:
+    - k: Slope of the S-N curve. Taken form DNV specification (k=3)
+    - C: Intercept parameter of the S-N curve. Taken from DNV specification (C=1.2E11)
+    - The S-N curve is defined as N = C*S^(-k) where S is the stress range
+    - Mo_w: List with spectral moments (from 0 to 4th) of the wave frequency (high frequency) component
+    - Mo_l: List with spectral moments (from 0 to 4th) of the low frequency component
+    - v0_w: Mean zero up crossing frequency of the wave frequency component
+    - v0_l: Mean zero up crossing frequency of the low frequency component
+    - Pxx_w: Power spectral density of the wave frequency component
+    - f_w: Frequency range of the wave frequency component
+    """
+   
+    # Damage due to small cycles:
+        
+    def pdf_rs(Rs, sigma_delta, sigma_HF):
+        prefactor = np.sqrt(2) / (np.sqrt(np.pi) * sigma_delta * sigma_HF**2)
+        exponent = - Rs**2 / (2 * sigma_HF**2)
+        
+        def integrand(y, Rs, sigma_delta, sigma_HF):
+            term1 = np.exp(-(1 / (2 * sigma_delta**2) + 1 / (2 * sigma_HF**2)) \
+                            * y**2 - 1 / (sigma_HF**2) * y * Rs)
+            return (y + Rs) * term1
+
+        pdf = integrate.quad(integrand, 0, Rs, args=(Rs, sigma_delta, sigma_HF))[0] \
+            * prefactor * np.exp(exponent)
+        return pdf
+    
+    sigma_HF = np.sqrt(Mo_w[0])
+    mu_hf = v0_w
+    sigma_delta = np.sqrt(2*Mo_l[0] - 2 * integrate.simps(Pxx_w * np.cos(f_w * mu_hf/2), f_w))
+    
+    small_integral = integrate.quad(lambda Rs:  Rs**k \
+                                    * pdf_rs(Rs, sigma_delta, sigma_HF), 0, np.inf)[0]
+    
+    d_small = 2**k * (v0_w - v0_l) / C * small_integral
+    
+    
+    # Damage due to large cycles:
+        
+    def pdf_rl(RL, sigma_delta, sigma_HF, sigma_LF):
+        prefactor = np.sqrt(2) / (np.sqrt(np.pi) * sigma_delta * sigma_HF**2 * sigma_LF**2)
+        exponent = - RL**2 / (2 * sigma_LF**2)
+        
+        def integrand(z, y, RL, sigma_LF, sigma_HF, sigma_delta):
+            term1 = (RL - z) * (y + z)
+            term2 = np.exp((-(1 / (2 * sigma_LF**2) + 1 / (2 * sigma_HF**2)) * z**2) \
+                            + (1 / sigma_LF**2) * RL * z - ((1 / (2 * sigma_delta**2)) \
+                            + (1 / (2 * sigma_HF**2))) * y**2 - (1 / (sigma_HF**2)) * y * RL)
+
+            return term1 * term2
+    
+
+        pdf = integrate.dblquad(integrand, 0, RL, 0, lambda z: z, 
+                                args=(RL, sigma_LF, sigma_HF, sigma_delta))[0] \
+            * prefactor * np.exp(exponent)            
+        return  pdf
+    
+    sigma_LF = np.sqrt(Mo_l[0])
+    
+    large_integral = integrate.quad(lambda Rl:  Rl**k * pdf_rl(Rl, sigma_delta,
+                                                                sigma_HF, sigma_LF), 0, np.inf)[0]
+    d_large = 2**k * v0_l / C * large_integral
+
+    # Total damage
+    d = d_small + d_large
+    return d
+
+
 ###############################################################################
 
 # d)	Combined fatigue damage – narrowband damage combination
@@ -530,8 +679,10 @@ def Lotsberg(k, C, Mo_w, Mo_l, ZUCF_w, ZUCF_l):
 def Huang_Moan(k, C, Mo_w, Mo_l, ZUCF_w, ZUCF_l):
     D_NB_HF = Narrowband(k, C, Mo_w, ZUCF_w)
     D_NB_LF = Narrowband(k, C, Mo_l, ZUCF_l)
-    d = ((D_NB_HF / ZUCF_w)**(2 / k) + (D_NB_LF / ZUCF_l)**(2 / k))**((k - 2) / 2) * (ZUCF_w**2 * (D_NB_HF / ZUCF_w)**(2 / k) + ZUCF_l**2 *
-                                                                                      (D_NB_LF / ZUCF_l)**(2 / k))**(3 / 2) / (ZUCF_w**4 * (D_NB_HF / ZUCF_w)**(2 / k) + ZUCF_l**4 * (D_NB_LF / ZUCF_l)**(2 / k))**(1 / 2)
+    d = ((D_NB_HF / ZUCF_w)**(2 / k) + (D_NB_LF / ZUCF_l)**(2 / k))**((k - 2) / 2) \
+        * (ZUCF_w**2 * (D_NB_HF / ZUCF_w)**(2 / k) + ZUCF_l**2 \
+           * (D_NB_LF / ZUCF_l)**(2 / k))**(3 / 2) / (ZUCF_w**4 * (D_NB_HF / ZUCF_w)**(2 / k) \
+           + ZUCF_l**4 * (D_NB_LF / ZUCF_l)**(2 / k))**(1 / 2)
     return d
 
 
@@ -560,6 +711,146 @@ def BandsMethod(k, C, tension, fs, nbands, cor = False):
         m0_i_ref = (f_ref/f_i[len(f_i)//2])**(2*beta)*m0_i
         m0 += m0_i_ref
         j += f_area
-    # d = (K * (2*alfa)**(1/beta) * f_ref / ((np.sqrt(m0))**(1/beta)))
-    d = (f_ref * K * (2*alfa)**(1/beta)) / ((2*np.sqrt(1*m0))**(1/beta))
+        
+    d = (f_ref * K * (2*alfa)**(1/beta)) / ((2*np.sqrt(m0))**(1/beta))
     return d
+
+
+
+def Gao_Zheng(k, C, f_l, Pxx_l, f_w, Pxx_w, v0_w, v0_l, v0):
+    def get_a_values(P, gamma):
+        if gamma >= 2 and gamma <= 4:
+            a_values = {
+                'P1': (-14.5495, -13.6189, -1.7305),
+                'P2': (46.3878, 27.1178, 2.5926),
+                'P3': (-10.851, 7.0653, -0.0151),
+                'P4': (-14.2695, -13.4855, -0.7394),
+                'P5': (0.1915, -0.0838, -0.0002),
+                'P6': (13.335, -9.663, 0.1395),
+                'P7': (203.4813, -3.2088, 6.4657),
+                'P8': (-0.0486, -11.2238, 3.1149),
+                'P9': (20.8779, -18.3121, -1.0359),
+                'P10': (10.325, 1.2937, 0.7107),
+                'P11': (56.3776, 7.792, -2.2805)
+            }
+        elif gamma > 4:
+            a_values = {
+                'P1': (0.1312, -0.0105, -0.0031),
+                'P2': (-0.1205, 0.0166, 0.0052),
+                'P3': (-0.0132, -0.0384, 0.0086),
+                'P4': (0.0442, -0.0133, -0.001),
+                'P5': (-0.01, 0.0095, -0.0008),
+                'P6': (-0.003, 0.0265, -0.0067),
+                'P7': (-0.6535, -0.1117, 0.0142),
+                'P8': (-0.1837, -0.0632, 0.0055),
+                'P9': (0.2871, -0.008, 0.0034),
+                'P10': (-0.0419, 0.0227, 0.0008),
+                'P11': (0.0393, 0.0577, -0.0033)
+            }
+        else:
+            print('The xi coefficient is not empirically fitted for the given gamma value')
+            return (0,0,0)
+        return a_values.get(P)
+
+    def xi_function(gamma, beta, k):
+        P = []
+        for n in range(1, 12):
+            a0, a1, a2 = get_a_values(f'P{n}', gamma)
+            P.append(a0 + a1 * k + a2 * k**2)
+
+        log_gamma, log_beta = np.log(gamma), np.log(beta)
+        xi = (np.dot(P[:6], [1, log_gamma, log_beta, log_gamma**2, log_beta**2, log_gamma*log_beta]) /
+              (1 + np.dot(P[6:], [log_gamma, log_beta, log_gamma**2, log_beta**2, log_gamma*log_beta])))
+        return xi
+
+    def spectralmom_sum(k, Pxx, f):
+        df = np.diff(f, prepend=f[0])
+        return np.sum(f[1:]**(2/k) * Pxx[1:] * df[1:])
+
+    def combined_spectralmom_sum(k, Pxx_1, f_1, Pxx_2, f_2, v0_1, v0_2):
+        beta = integrate.simps(Pxx_2, f_2) / integrate.simps(Pxx_1, f_1)
+        gamma = v0_1 / v0_2
+        xi = xi_function(gamma, beta, k)
+        df_1, df_2 = np.diff(f_1, prepend=f_1[0]), np.diff(f_2, prepend=f_2[0])
+        lamba_2_k = np.sum(xi * np.sqrt((f_1[1:]**(2/k) * Pxx_1[1:] * df_1[1:]) *
+                                        (f_2[1:]**(2/k) * Pxx_2[1:] * df_2[1:])))
+        return lamba_2_k
+
+    lamba_2_k_lf = spectralmom_sum(k, Pxx_l, f_l)
+    lamba_2_k_hf = spectralmom_sum(k, Pxx_w, f_w)
+    lamba_2_k_lf_hf = combined_spectralmom_sum(k, Pxx_l, f_l, Pxx_w, f_w, v0_w, v0_l)
+
+    d = (2*np.sqrt(2))**k / C * gamma_func(1 + k/2) * (lamba_2_k_lf + lamba_2_k_hf + lamba_2_k_lf_hf)**(k/2)
+
+    return d
+
+
+
+def ImprovedBandsMethod(k, C, tension_low, tension_high, fs, nbands, Mo_w, Mo_l):
+    # Compute the PSD using Welch's method
+    f_l, Pxx_l = sig.welch(tension_low, fs=fs, nperseg=100 * nbands - 1)
+    f_h, Pxx_h = sig.welch(tension_high, fs=fs, nperseg=100 * nbands - 1)
+    f_l_r, f_h_r = f_l[len(f_l)//2], f_h[len(f_h)//2]
+   
+    d_w = np.sqrt(1 - Mo_w[1]**2 / (Mo_w[0] * Mo_w[2]))  # Vanmarcke bandwidth parameter
+    
+    f_area = len(f_l) // nbands
+    m0_r_lf, m0_r_hf = 0, 0
+    for j in range(0, len(f_l), f_area):
+        f_i_l, Pxx_i_l = f_l[j:j+f_area], Pxx_l[j:j+f_area]
+        m0_l_i = per_spectral_mom(f_i_l, Pxx_i_l, n=[0])[0]
+        m0_l_i_ref = (f_l_r / f_i_l[len(f_i_l)//2])**(-2/k) * m0_l_i
+        m0_r_lf += m0_l_i_ref
+        
+        f_i_h, Pxx_i_h = f_h[j:j+f_area], Pxx_h[j:j+f_area]
+        m0_h_i = per_spectral_mom(f_i_h, Pxx_i_h, n=[0])[0]
+        m0_h_i_ref = (f_h_r / f_i_h[len(f_i_h)//2])**(-2/k) * m0_h_i
+        m0_r_hf += m0_h_i_ref
+
+    
+    def get_P_values_for_k(k, vanmarcke_hf):
+        
+        if vanmarcke_hf <= 0.1:
+            P_values = {
+                'P1': {3: 1.118, 4: 1.112, 5: 1.108},
+                'P2': {3: -0.5818, 4: -0.6308, 5: -0.6301},
+                'P3': {3: 0.1947, 4: 0.1213, 5: 0.06409},
+                'P4': {3: 0.1369, 4: 0.1372, 5: 0.1274},
+                'P5': {3: 0.05659, 4: 0.05294, 5: 0.0442},
+                'P6': {3: 0.03104, 4: 0.03733, 5: 0.03809},
+                'P7': {3: -0.4979, 4: -0.5555, 5: -0.5622},
+                'P8': {3: 0.2341, 4: 0.1689, 5: 0.1178},
+                'P9': {3: 0.1242, 4: 0.1282, 5: 0.121},
+                'P10': {3: 0.0525, 4: 0.04615, 5: 0.03535},
+                'P11': {3: 0.01831, 4: 0.02246, 5: 0.02295}
+            }
+        else:
+            P_values = {
+                'P1': {3: 1.021, 4: 0.9841, 5: 0.927},
+                'P2': {3: -0.289, 4: -0.2251, 5: -0.09982},
+                'P3': {3: 0.1204, 4: 0.1132, 5: 0.1266},
+                'P4': {3: 0.1964, 4: 0.1524, 5: 0.1536},
+                'P5': {3: 0.07312, 4: 0.08618, 5: 0.1189},
+                'P6': {3: 0.1692, 4: 0.1541, 5: 0.1874},
+                'P7': {3: -0.3553, 4: -0.3301, 5: -0.2593},
+                'P8': {3: 0.1524, 4: 0.1477, 5: 0.1458},
+                'P9': {3: 0.2125, 4: 0.1759, 5: 0.1813},
+                'P10': {3: 0.06772, 4: 0.07799, 5: 0.1097},
+                'P11': {3: 0.1667, 4: 0.1591, 5: 0.2111}
+            }
+        
+        return [P_values[f'P{i}'][k] for i in range(1, 12)]
+    
+    P = get_P_values_for_k(k, d_w)
+    gamma, beta = f_h_r / f_l_r, Mo_w[0] / Mo_l[0]
+    
+    log_gamma, log_beta = np.log(gamma), np.log(beta)
+    mu_numerator = P[0] + P[1]*log_gamma + P[2]*log_beta + P[3]*log_gamma**2 + P[4]*log_beta**2 + P[5]*log_gamma*log_beta
+    mu_denominator = 1 + P[6]*log_gamma + P[7]*log_beta + P[8]*log_gamma**2 + P[9]*log_beta**2 + P[10]*log_gamma*log_beta
+    mu = mu_numerator / mu_denominator
+    
+    m0_total = m0_r_lf + gamma**(2/k) * m0_r_hf * mu
+    d = f_l_r / C * (2 * np.sqrt(2 * m0_total))**k * gamma_func(k / 2 + 1)
+    return d
+
+
